0b0754e4d30d521f59d478d33aba870820223530
michael-conway <michael_conway@unc.edu>
2016-04-20 10:35:43 -0600

    #1 init skeleton

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+# Project: Publisher framework for DE
+
+### Date:
+### Release Version: 1.0.0-SNAPSHOT
+### git tag: 
+#### Developer: Mike Conway - DICE
+
+(work in progress)
---
+++ src/main/java/org/dfc/de/App.java
@@ -0,0 +1,13 @@
+package org.dfc.de;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
