57aa51f51763408ecfff47a3dbed6ea0fa9a623d
michael-conway <michael_conway@unc.edu>
2016-04-22 10:18:06 -0400

    #1 working on api

---
+++ src/main/java/org/iplantc/de/publish/mechanism/api/PublishMechanism.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package org.iplantc.de.publish.mechanism.api;
+
+/**
+ * Describes a publish mechanism, this is a plugin that can format and publish
+ * collection data sorted as an AIP in the DE/iRODS environment as a DIP in an
+ * arbitrary target repository. The mechanism is a simple api that is developed
+ * underneath this interface, and can surface a preValidate foreground method
+ * that can do sanity checks and pre-processing, and a sych|asynch publish
+ * action that actually creates the DIP in the target repository.
+ * 
+ * @author Mike Conway - DICE, Dennis Roberts, CyVerse
+ *
+ */
+public interface PublishMechanism {
+
+	/**
+	 * Do a pre-validate that may check for all necessary requirements, and may
+	 * also pre-process the data before publishing. This is necessary as the
+	 * actual publish phase may be run asynchronously as an app. Having an
+	 * initial foreground step thus gives a hook for a publish mechanism that
+	 * needs to itneract with iRODS or DE.
+	 * <p/>
+	 * Note that, depending on the mechanism, the publish action can also run in
+	 * the foreground, and this may be suitable for light-weight publishing
+	 * workflows.
+	 * <p/>
+	 * Note that the publish action should trap any checked or unchecked
+	 * exception and internally convert it into a <code>PublishResult</code> so
+	 * that the publish mechanism can handle exception and validation logic.
+	 * 
+	 * @param publishActionDescriptor
+	 *            {@link PublishActionDescriptor} that provides the necessary
+	 *            context for the mechanism to find and do the publishing.
+	 * @return {@link PublishResult} That represents the status and any error or
+	 *         validation information
+	 */
+	public PublishResult preValidate(
+			final PublishActionDescriptor publishActionDescriptor);
+
+	/**
+	 * This method will be called after success in the <code>preValidate</code>
+	 * and represents the actual publish action. This may be done synchronously,
+	 * or the result can reflect an asynchronous submission of a publication
+	 * action. Asynchronous actions are handled through the DE App abstraction,
+	 * and results are processed using the notification and error handling logic
+	 * as it exists. Publish mechansims can also handle exceptions and any
+	 * required clean-up witin this publish mechanism.
+	 * 
+	 * @param publishActionDescriptor
+	 *            {@link PublishDescriptor} with the necessary context for an
+	 *            individual publicastion action
+	 * @return {@link PublishResult} with the result of this publish action. For
+	 *         synchronous actions, this may be the failure or completion
+	 *         success, for an asynchyronous action, it would represent the
+	 *         success or failure in enquing the the publish action.
+	 */
+	public PublishResult publish(
+			final PublishActionDescriptor publishActionDescriptor);
+
+}
---
+++ src/main/java/org/iplantc/de/publish/mechanism/api/PublishResult.java
@@ -0,0 +1,205 @@
+/**
+ * 
+ */
+package org.iplantc.de.publish.mechanism.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Describes the result of a pre-publish or publish action. This can include
+ * status information, and any validation messages, including an overall result
+ * as well as the ability to pass validation information per parameter
+ * 
+ * @author Mike Conway - DICE
+ *
+ */
+public class PublishResult {
+
+	/**
+	 * Represents the phase of the publishing cycle that this response pertains
+	 * to
+	 */
+	private PublishPhaseEnum publishPhase;
+	/**
+	 * Enum value that represents the status at this particular phase
+	 */
+
+	/**
+	 * Represents an intermediate location for a DIP that may have been created
+	 * during the pre-process phase. This allows a publish mechanism to publish
+	 * from a location that is different than the original source. For example,
+	 * the preprocess step may have created a new arrangement, or it may have
+	 * created some sort of bag or archive file.
+	 * <p/>
+	 * The publish system will pass this intermediate DIP location between the
+	 * result of the validate and the publication step.
+	 */
+	private String intermediateDipAbsolutePath;
+
+	private PublishStatusEnum publishStatus;
+
+	/**
+	 * Numeric response code that may be particular to the publish mechanism
+	 */
+	private int responseCode = 0;
+
+	/**
+	 * Overall free text response for the publish result
+	 */
+	private String responseMessage = "";
+
+	/**
+	 * Sequential list of log or validation messages
+	 */
+	private List<String> messageLog = new ArrayList<String>();
+
+	/**
+	 * Optional stack trace information
+	 */
+	private String stackTrace = "";
+
+	/**
+	 * @return the publishPhase
+	 */
+	public PublishPhaseEnum getPublishPhase() {
+		return publishPhase;
+	}
+
+	/**
+	 * @param publishPhase
+	 *            the publishPhase to set
+	 */
+	public void setPublishPhase(PublishPhaseEnum publishPhase) {
+		this.publishPhase = publishPhase;
+	}
+
+	/**
+	 * @return the publishStatus
+	 */
+	public PublishStatusEnum getPublishStatus() {
+		return publishStatus;
+	}
+
+	/**
+	 * @param publishStatus
+	 *            the publishStatus to set
+	 */
+	public void setPublishStatus(PublishStatusEnum publishStatus) {
+		this.publishStatus = publishStatus;
+	}
+
+	/**
+	 * @return the responseCode
+	 */
+	public int getResponseCode() {
+		return responseCode;
+	}
+
+	/**
+	 * @param responseCode
+	 *            the responseCode to set
+	 */
+	public void setResponseCode(int responseCode) {
+		this.responseCode = responseCode;
+	}
+
+	/**
+	 * @return the responseMessage
+	 */
+	public String getResponseMessage() {
+		return responseMessage;
+	}
+
+	/**
+	 * @param responseMessage
+	 *            the responseMessage to set
+	 */
+	public void setResponseMessage(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}
+
+	/**
+	 * @return the messageLog
+	 */
+	public List<String> getMessageLog() {
+		return messageLog;
+	}
+
+	/**
+	 * @param messageLog
+	 *            the messageLog to set
+	 */
+	public void setMessageLog(List<String> messageLog) {
+		this.messageLog = messageLog;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		final int maxLen = 10;
+		StringBuilder builder = new StringBuilder();
+		builder.append("PublishResult [");
+		if (publishPhase != null) {
+			builder.append("publishPhase=").append(publishPhase).append(", ");
+		}
+		if (intermediateDipAbsolutePath != null) {
+			builder.append("intermediateDipAbsolutePath=")
+					.append(intermediateDipAbsolutePath).append(", ");
+		}
+		if (publishStatus != null) {
+			builder.append("publishStatus=").append(publishStatus).append(", ");
+		}
+		builder.append("responseCode=").append(responseCode).append(", ");
+		if (responseMessage != null) {
+			builder.append("responseMessage=").append(responseMessage)
+					.append(", ");
+		}
+		if (messageLog != null) {
+			builder.append("messageLog=")
+					.append(messageLog.subList(0,
+							Math.min(messageLog.size(), maxLen))).append(", ");
+		}
+		if (stackTrace != null) {
+			builder.append("stackTrace=").append(stackTrace);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
+	/**
+	 * @return the stackTrace
+	 */
+	public String getStackTrace() {
+		return stackTrace;
+	}
+
+	/**
+	 * @param stackTrace
+	 *            the stackTrace to set
+	 */
+	public void setStackTrace(String stackTrace) {
+		this.stackTrace = stackTrace;
+	}
+
+	/**
+	 * @return the intermediateDipAbsolutePath
+	 */
+	public String getIntermediateDipAbsolutePath() {
+		return intermediateDipAbsolutePath;
+	}
+
+	/**
+	 * @param intermediateDipAbsolutePath
+	 *            the intermediateDipAbsolutePath to set
+	 */
+	public void setIntermediateDipAbsolutePath(
+			String intermediateDipAbsolutePath) {
+		this.intermediateDipAbsolutePath = intermediateDipAbsolutePath;
+	}
+
+}
