c7de81650b53a80ac2162d188779f86c00663dd0
michael-conway <michael_conway@unc.edu>
2016-05-26 13:23:10 -0700

    #2 add dummy publisher

--- publisher-sandbox/src/main/java/org/iplantc/de/publish/sandbox/PublisherEmulator.java
+++ publisher-sandbox/src/main/java/org/iplantc/de/publish/sandbox/PublisherEmulator.java
@@ -3,6 +3,10 @@
  */
 package org.iplantc.de.publish.sandbox;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+
 import org.iplantc.de.publish.mechanism.api.exception.PublicationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,6 +60,31 @@ public class PublisherEmulator {
 					"init() cannot be called, no provided sandboxConfiguration");
 		}
 
+		if (sandboxConfiguration.getJarFilePluginDir() == null
+				|| sandboxConfiguration.getJarFilePluginDir().isEmpty()) {
+			throw new PublicationException(
+					"no jar file plugin directory specified");
+		}
+
+		log.info("scanning for plugin jars at:{}",
+				sandboxConfiguration.getJarFilePluginDir());
+
+	}
+
+	public void initialize(String libDir) throws Exception {
+		File dependencyDirectory = new File(libDir);
+		File[] files = dependencyDirectory.listFiles();
+		ArrayList<URL> urls = new ArrayList<URL>();
+		for (int i = 0; i < files.length; i++) {
+			if (files[i].getName().endsWith(".jar")) {
+				urls.add(files[i].toURL());
+				// urls.add(files[i].toURI().toURL());
+			}
+		} /*
+		 * classLoader = new JarFileClassLoader("Scheduler CL" +
+		 * System.currentTimeMillis(), urls.toArray(new URL[urls.size()]),
+		 * GFClassLoader.class.getClassLoader());
+		 */
 	}
 
 }
